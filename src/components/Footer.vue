4b610ed214a333f34a45d06f436dc07ad41ead4c
ingeniero26 <ingjerson2014@gmail.com>
2022-09-12 20:38:26 -0500

    columnas en el footer, mejoras en about y en home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <footer class="footer">
+            <div class="columns is-mobile">
+                <div class="column">
+                    <p>Enlaces</p>
+                    <a href="">Login</a>
+                </div>
+                <div class="column">Registros</div>
+                <div class="column">Terminos</div>
+                <div class="column">Redes</div>
+            </div>
             <div class="content has-text-centered">
                 <p>
                     <strong>Desarrollo</strong> by
